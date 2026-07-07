45d118c75ef18f5a0048730e0125dedbe6480475
Franky-maker <francisfregeau6@gmail.com>
2026-07-07 00:45:27 -0400

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-// Petit remplacement de window.storage pour que le site fonctionne
-// correctement en dehors de Claude (les commandes partent par email).
 if (!window.storage) {
   window.storage = {
     get: async () => null,
